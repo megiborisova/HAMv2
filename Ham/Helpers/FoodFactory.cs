0d54ee46a83440e29695d7c44e3fe9a54343103a
alexanderchernev <a.chernev009@gmail.com>
2025-06-11 22:27:00 +0300

    FoodFactory and Helpers Added

---
+++ Ham/Helpers/FoodFactory.cs
@@ -0,0 +1,38 @@
+using CalorieTracker.Foods;
+using System;
+
+namespace CalorieTracker.Factory
+{
+    public static class FoodFactory
+    {
+        public static Food? CreateFood(string category, string name, int calories, double carbs, double protein, double fats, 
+                                       double? volume = null, double? alcoholPercent = null)
+        {
+            switch (category.ToLower())
+            {
+                case "fruit":
+                    return new Fruit(name, calories, carbs, protein, fats);
+                case "vegetable":
+                    return new Vegetable(name, calories, carbs, protein, fats);
+                case "alcoholic":
+                    if (volume.HasValue && alcoholPercent.HasValue)
+                        return new Alcoholic(name, calories, volume.Value, alcoholPercent.Value, carbs, protein, fats);
+                    break;
+                case "nonalcoholic":
+                    if (volume.HasValue)
+                        return new NonAlcoholic(name, calories, volume.Value, carbs, protein, fats);
+                    break;
+                case "meat":
+                    return new Meat(name, calories, carbs, protein, fats);
+                case "dairy":
+                    return new Dairy(name, calories, carbs, protein, fats);
+                case "otheranimalbased":
+                    return new OtherAnimalProduct(name, calories, carbs, protein, fats);
+                default:
+                    Console.WriteLine("Unknown category.");
+                    break;
+            }
+            return null;
+        }
+    }
+}
---
+++ Ham/Helpers/Helpers.cs
@@ -0,0 +1,37 @@
+namespace CalorieTracker.Helpers
+{
+    class Helpers
+    {
+        public static double ReadDouble(string message)
+        {
+            double value;
+            do
+            {
+                Console.Write(message);
+            } while (!double.TryParse(Console.ReadLine(), out value) || value < 0);
+            return value;
+        }
+        
+
+        public static int ReadInt(string message)
+        {
+            int value;
+            do
+            {
+                Console.Write(message);
+            } while (!int.TryParse(Console.ReadLine(), out value) || value < 0);
+            return value;
+        }
+        public static string ReadString(string message)
+        {
+            string? input;
+            do
+            {
+                Console.Write(message);
+                input = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(input));
+            return input;
+        }
+    }
+}
+
