1a768969c822e8d8526664dc640c45e5f7549f1e
alexanderchernev <a.chernev009@gmail.com>
2025-06-11 22:43:17 +0300

    Animal Based Added

---
+++ Ham/Food/Solid/AnimalBased/Other/Other.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CalorieTracker.Foods
+{
+    public class OtherAnimalProduct : AnimalBased
+    {
+        public OtherAnimalProduct() { }
+
+        public OtherAnimalProduct(string name, int calories, double carbs, double protein, double fats)
+            : base(name, calories, carbs, protein, fats) { }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} - {Calories} kcal (Other Animal-Based) - {Carbs}g carbs, {Protein}g protein, {Fats}g fats");
+        }
+    }
+}
